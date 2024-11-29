af01c0e51111769a11f5a8e429a764577e755a0f
Frank <andreass.frankk@gmail.com>
2024-11-29 17:51:50 +0200

    fixed loders

--- src/app/top-losers/page.tsx
+++ src/app/top-losers/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-[24px] pb-20 gap-16  font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full max-w-[1200px]">
         <div className="flex justify-between items-center w-full mb-6">
-          <h1 className="text-[30px] font-bold  mb-4">Top 100 Gainers</h1>
+          <h1 className="text-[30px] font-bold  mb-4">Top 100 Losers</h1>
           <div>
             <FormControl>
               <FormLabel id="demo-row-radio-buttons-group-label">
